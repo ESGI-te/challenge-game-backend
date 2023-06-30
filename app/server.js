f4ac6d8eb4461468a2240ca87fba45848cb84f3c
Echaimi <echaimi@myges.fr>
2023-06-30 23:00:55 +0200

    authentificate

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const UserRouter = require("./routes/user.router");
 const QuizzRouter = require("./routes/quizz.router");
+const AuthRouter = require("./routes/auth.router");
 const errorsHandler = require("./middlewares/errorHandler");
 const dotenv = require("dotenv");
 
@@ -12,20 +13,20 @@ require("./db")();
 app.use(express.json());
 
 app.use("/users", new UserRouter());
-
 app.use("/quizzs", new QuizzRouter());
+app.use("/", new AuthRouter());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+  res.send("Hello World!");
 });
 
 app.post("/", (req, res) => {
-	console.log(req.body);
-	res.send("Got a POST request");
+  console.log(req.body);
+  res.send("Got a POST request");
 });
 
 app.use(errorsHandler);
 
 app.listen(3000, () => {
-	console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000!");
 });
