a12b8c5d6741d85b1d0ef360ab020edc49b4c3e1
tlugat <teo.lugat@ausha.co>
2023-07-12 21:00:03 +0200

    ✨ User invitation socket

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
 	WS_LOBBY_NAMESPACE: "/lobby",
 	WS_GAME_NAMESPACE: "/game",
+	WS_FRIEND_INVITATION_NAMESPACE: "/friend-request",
 
 	USER_ROLES: {
 		ADMIN: "admin",
