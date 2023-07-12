dcd6273c37e7084403b84d0c1c96c0a7af35104a
tlugat <teo.lugat@ausha.co>
2023-07-12 14:13:20 +0200

    ✨ Auth guard : JWT auth and auth role middlewares

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
+const { USER_ROLES } = require("../utils/constants");
 
 /**
  * User schema
  */
 
 const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
-    match: [
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please enter a valid email",
-    ],
-  },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		// Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
+		match: [
+			/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			"Please enter a valid email",
+		],
+	},
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	roles: {
+		type: [String],
+		required: true,
+		enum: Object.values(USER_ROLES),
+		default: [USER_ROLES.USER],
+	},
 });
 
 /**
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,4 +1,9 @@
 module.exports = Object.freeze({
 	WS_LOBBY_NAMESPACE: "/lobby",
 	WS_GAME_NAMESPACE: "/game",
+
+	USER_ROLES: {
+		ADMIN: "admin",
+		USER: "user",
+	},
 });
