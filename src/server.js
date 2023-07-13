8875d03e4ddb8d78586a7620d6340a357a3296af
tlugat <teo.lugat@ausha.co>
2023-07-13 10:50:57 +0200

    ♻️ Various refacto

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const UserRouter = require("./routes/user.router");
+const QuizzRouter = require("./routes/quizz.router");
+const GameRouter = require("./routes/game.router");
+const LobbyRouter = require("./routes/lobby.router");
+const SecurityRouter = require("./routes/security.router");
+const UserInvitationRouter = require("./routes/userInvitation.router");
+
+const GameSocket = require("./websockets/game.ws");
+const LobbySocket = require("./websockets/lobby.ws");
+const UserInvitationSocket = require("./websockets/userInvitation.ws");
+
+const errorsHandler = require("./middlewares/errorHandler");
+const authGuard = require("./middlewares/auth");
+
+const dotenv = require("dotenv");
+const wsAuthGuard = require("./middlewares/wsAuth");
+
+const app = express();
+
+dotenv.config();
+require("./db")();
+
+app.use(cors());
+
+const server = app.listen(3000, () => {
+	console.log("App listening on port 3000!");
+});
+
+const io = require("./socket")(server);
+
+io.use(wsAuthGuard);
+
+GameSocket(io);
+LobbySocket(io);
+UserInvitationSocket(io);
+
+app.use(express.json());
+
+app.use("/", SecurityRouter());
+
+app.use(authGuard);
+
+app.use("/users", UserRouter());
+app.use("/quizzs", QuizzRouter());
+app.use("/games", GameRouter());
+app.use("/lobbies", LobbyRouter());
+app.use("/friend-invitations", UserInvitationRouter());
+
+app.use(errorsHandler);
---
+++ src/websockets/lobby.ws.js
@@ -0,0 +1,50 @@
+const SecurityService = require("../services/security.service");
+const LobbyService = require("../services/lobby.service");
+const { WS_LOBBY_NAMESPACE } = require("../utils/constants");
+
+module.exports = (io) => {
+	const lobbyService = LobbyService();
+	const securityService = SecurityService();
+
+	const namespace = io.of(WS_LOBBY_NAMESPACE);
+
+	const handleConnection = async (socket) => {
+		const lobbyId = socket.handshake.query["lobbyId"];
+		const userId = socket.handshake.query["userId"];
+		const token = socket.handshake.auth.token;
+
+		const player = await securityService.getUserFromToken(token);
+		const lobby = await lobbyService.addPlayer(lobbyId, player);
+
+		socket.join(lobbyId);
+
+		namespace.to(lobbyId).emit("notification", {
+			title: "Someone's here",
+			description: `${player.username} just joined the lobby`,
+		});
+
+		namespace.to(lobbyId).emit("players", lobby.players);
+
+		if (lobby.players.length === lobby.playersMax) {
+			namespace.to(lobbyId).emit("game_start", lobby.gameId);
+			socket.leave(lobbyId);
+			lobbyService.deleteOne(lobbyId);
+		}
+
+		socket.on("new_message", (msg) => {
+			namespace.to(lobbyId).emit("message", { player: player.username, msg });
+		});
+
+		socket.on("disconnect", async () => {
+			socket.leave(lobbyId);
+			const { players } = await lobbyService.removePlayer(lobbyId, userId);
+			namespace.to(lobbyId).emit("notification", {
+				title: "Someone just left",
+				description: `${player.username} just left the lobby`,
+			});
+			namespace.to(lobbyId).emit("players", players);
+		});
+	};
+
+	namespace.on("connection", handleConnection);
+};
