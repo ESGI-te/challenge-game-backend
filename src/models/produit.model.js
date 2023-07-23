85e88c4d7459611171cb396dfc853c7bb8a48ed5
DrAtsiSama <dareka@outlook.fr>
2023-07-23 16:50:17 +0200

    WIP

--- src/models/produit.model.js
+++ src/models/produit.model.js
@@ -6,24 +6,65 @@ const mongoose = require("mongoose");
 
 const produitSchema = new mongoose.Schema({
   nom: { type: String, required: true },
-  description: { type: String },
-  prix: { type: Number, required: true },
-  quantite: { type: Number, default: 0 },
+  description: { type: String, required: true },
+  prix: {
+    argent: { type: Number, default: 0 }, // Prix en euros
+    pointsCultures: { type: Number, default: 0 }, // Prix en gemmes
+  },
+  quantiteEnStock: { type: Number, default: -1 }, // -1 = une infinité
 });
-
 
 /**
  * Methods
  */
 
-ProduitSchema.methods = {};
+produitSchema.methods = {};
 
 /**
  * Statics
  */
 
-ProduitSchema.statics = {};
+produitSchema.statics = {};
 
-const Produit = mongoose.model("Produit", ProduitSchema);
+const Produit = mongoose.model("Product", produitSchema);
+
+// Exemple de création d'un produit
+const nouveauProduit = new Produit({
+  nom: 'Produit Mixte',
+  description: 'Ceci est un produit avec un prix en euros et en gemmes',
+  prix: {
+    argent: 19.99,
+    pointsCultures: 100,
+  },
+  quantiteEnStock: 50,
+});
+
+// Sauvegarder le produit dans la base de données
+nouveauProduit.save()
+  .then((produit) => {
+    console.log('Produit sauvegardé :', produit);
+  })
+  .catch((err) => {
+    console.error('Erreur lors de la sauvegarde du produit :', err);
+  });
+
+  // Exemple de création d'un produit
+const nouveauProduit2 = new Produit({
+  nom: 'Produit Infini',
+  description: 'Ceci est un produit avec un prix en euros et en gemmes',
+  prix: {
+    argent: 19.99,
+    pointsCultures: 100,
+  },
+});
+
+// Sauvegarder le produit dans la base de données
+nouveauProduit2.save()
+  .then((produit) => {
+    console.log('Produit sauvegardé :', produit);
+  })
+  .catch((err) => {
+    console.error('Erreur lors de la sauvegarde du produit :', err);
+  });
 
 module.exports = Produit;
