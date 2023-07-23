188306cdabf680f964e5975d3514f56e99491da2
Chakib <karmimchakib@gmail.com>
2023-07-23 19:22:00 +0200

    send sucess and cancel urls from vue

--- src/controllers/stripe.controller.js
+++ src/controllers/stripe.controller.js
@@ -1,30 +1,37 @@
 module.exports = (options = {}) => {
-	const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+  const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
-	return {
-		async createCheckoutSession(req, res) {
-			const items =
-				[{
-					price_data: {
-						currency: 'eur',
-						product_data: {
-						  name: req.body.name,
-						},
-						unit_amount: req.body.price,
-					  },
-					  quantity: req.body.quantity,
-				}];
-			
-			console.log(items);
-			const session = await stripe.checkout.sessions.create({
-				payment_method_types: ["card"],
-				line_items: items,
-				mode: "payment",
-				success_url: `${process.env.CLIENT_URL}/payment/success`,
-				cancel_url: `${process.env.CLIENT_URL}/payment/cancel`,
-			});
+  return {
+    async createCheckoutSession(req, res) {
+      try {
+        const items = [
+          {
+            price_data: {
+              currency: "eur",
+              product_data: {
+                name: req.body.product.name,
+              },
+              unit_amount: req.body.product.price,
+            },
+            quantity: req.body.product.quantity,
+          },
+        ];
 
-			res.json({ id: session.id });
-		},
-	};
+        
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ["card"],
+          line_items: items,
+          mode: "payment",
+          success_url: req.body.product.success_url,
+          cancel_url: req.body.product.cancel_url,
+        });
+
+        res.json({ id: session.id });
+        // res.json(session);
+
+      } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+      }
+    },
+  };
 };
