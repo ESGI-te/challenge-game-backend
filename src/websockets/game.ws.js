bdcae6d303b5aa4514987a0dce869ee20c27f726
Echaimi <echaimi@myges.fr>
2023-07-24 07:31:40 +0200

    ✨game question Logique

--- src/websockets/game.ws.js
+++ src/websockets/game.ws.js
@@ -10,22 +10,26 @@ module.exports = function (io) {
 
   const handleConnection = async (socket) => {
     const gameId = socket.handshake.query["gameId"];
-    const userId = socket.handshake.query["userId"];
-    const token = socket.handshake.auth.token;
-
-    const player = await securityService.getUserFromToken(token);
+    const { token } = socket.handshake.auth;
+    const user = await securityService.getUserFromToken(token);
     const game = await gameService.addPlayer(gameId, {
-      id: userId,
-      username: player.username,
+      _id: user._id,
+      username: user.username,
+      score: 0,
+      lives: 3,
     });
+    const updatedGame = await gameService.findOne(gameId);
 
     socket.join(gameId);
 
+    // console.log(updatedGame);
     namespace.to(gameId).emit("notification", {
       title: "Someone's here",
-      description: `${player.username} just joined the game`,
+      description: `${user.username} just joined the game`,
     });
-    namespace.to(gameId).emit("players", game.players);
+    if (updatedGame && updatedGame.user) {
+      namespace.to(gameId).emit("user", updatedGame.user);
+    }
 
     socket.on("answer", async ({ questionId, answer }) => {
       const isCorrect = await gameService.checkAnswer(
@@ -34,71 +38,65 @@ module.exports = function (io) {
         answer
       );
       if (isCorrect) {
-        await gameService.incrementScore(gameId, userId, 1);
+        await gameService.incrementScore(gameId, user._id, 1);
       } else {
-        await gameService.decrementLives(gameId, userId);
-        const lives = await gameService.getLives(gameId, userId);
+        await gameService.decrementLives(gameId, user._id);
+        const lives = await gameService.getLives(gameId, user._id);
         if (lives === 0) {
           socket.emit("eliminated");
           socket.leave(gameId);
-          const updatedGame = await gameService.eliminatePlayer(gameId, userId);
-          namespace.to(gameId).emit("player_eliminated", player.username);
+          const updatedGame = await gameService.eliminatePlayer(
+            gameId,
+            user._id
+          );
+          namespace.to(gameId).emit("player_eliminated", user.username);
         }
       }
     });
 
-    socket.on("vote_category", async ({ category }) => {
-      await gameService.voteCategory(gameId, userId, category);
-      const votedCategories = await gameService.getVotedCategories(gameId);
-      namespace.to(gameId).emit("voted_categories", votedCategories);
-
-      const playersCount = game.players.length;
-      const votedPlayersCount = votedCategories.length;
-
-      if (playersCount === votedPlayersCount || hasReachedTimeout()) {
-        const winningCategory = await gameService.getWinningCategory(gameId);
-        namespace.to(gameId).emit("category_chosen", winningCategory);
-        await startGame(gameId);
-      }
+    socket.on("start_game", async () => {
+      const updatedGame = await gameService.findOne(gameId);
+      startGame(gameId, updatedGame);
     });
 
-    const hasReachedTimeout = () => {
-      const votingTimeout = 45 * 1000;
-      const elapsedTime = new Date() - game.currentQuestionStartTime;
-      return elapsedTime >= votingTimeout;
-    };
+    const startGame = async (gameId, updatedGame) => {
+      const themes = await gameService.getQuestionsForPopularTheme(gameId);
 
-    const startGame = async (gameId) => {
-      let question = await gameService.getNextQuestion(gameId);
-      namespace.to(gameId).emit("question", question);
+      let allQuizzes = [];
+      themes.forEach((theme) => {
+        for (const level in theme.quizz) {
+          allQuizzes = allQuizzes.concat(theme.quizz[level]);
+        }
+      });
+
+      let question = allQuizzes[0];
+      console.log("First question: ", question);
+      namespace.to(gameId).emit("question", question.question); // Sending only the question text
+
+      let questionIndex = 1;
 
       const interval = setInterval(async () => {
-        question = await gameService.getNextQuestion(gameId);
-        if (question) {
-          namespace.to(gameId).emit("question", question);
+        if (questionIndex < allQuizzes.length) {
+          question = allQuizzes[questionIndex];
+          console.log("Next question: ", question);
+          namespace.to(gameId).emit("question", question.question); // Sending only the question text
+          questionIndex += 1;
         } else {
-          const alivePlayers = await gameService.getAlivePlayers(gameId);
-          if (alivePlayers.length > 1) {
-            clearInterval(interval);
-            namespace.to(gameId).emit("choose_category");
-          } else {
-            clearInterval(interval);
-            const winner = gameService.getWinner(gameId);
-            namespace.to(gameId).emit("game_over", winner);
-          }
+          clearInterval(interval);
+          const winner = await gameService.getWinner(gameId);
+          namespace.to(gameId).emit("game_over", winner);
         }
-      }, 30000);
+      }, updatedGame.settings.questionTime * 1000);
     };
-
     socket.on("disconnect", async () => {
       socket.leave(gameId);
       const updatedGame = await gameService.removePlayer(gameId, {
-        id: userId,
-        username: player.username,
+        _id: user._id,
+        username: user.username,
       });
       namespace.to(gameId).emit("notification", {
         title: "Someone just left",
-        description: `${player.username} just left the game`,
+        description: `${user.username} just left the game`,
       });
       namespace.to(gameId).emit("players", updatedGame.players);
     });
