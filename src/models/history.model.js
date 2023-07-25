3f0c19590a6a2df3cf89e26cbb32507b80a844da
Chakib <karmimchakib@gmail.com>
2023-07-25 17:19:05 +0200

    game stats and history

---
+++ src/models/history.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+/**
+ * Quizz schema
+ */
+
+const HistorySchema = new mongoose.Schema({
+  games: [
+    {
+      gameStatsId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "GameStats",
+        required: true,
+      },
+      rank: { type : Number, required: true },
+      score : { type : Number, required: true },
+      lives : { type : Number, required: true }
+    },
+  ],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+});
+
+/**
+ * Methods
+ */
+
+HistorySchema.method({});
+
+/**
+ * Statics
+ */
+
+HistorySchema.static({});
+
+module.exports = mongoose.model("History", HistorySchema);
