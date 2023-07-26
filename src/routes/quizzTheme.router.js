e882dc1cdd00b78743166ced6ded7548eccf0e41
Chakib <karmimchakib@gmail.com>
2023-07-26 17:05:30 +0200

    Add role guard to certain routes

--- src/routes/quizzTheme.router.js
+++ src/routes/quizzTheme.router.js
@@ -1,17 +1,18 @@
 module.exports = (options = {}) => {
-	const { Router } = require("express");
-	const router = Router();
-	const QuizzThemeService = require("../services/quizzTheme.service");
-	const QuizzThemeController = require("../controllers/quizzTheme.controller");
-	const controller = QuizzThemeController(QuizzThemeService());
+  const { Router } = require("express");
+  const router = Router();
+  const QuizzThemeService = require("../services/quizzTheme.service");
+  const QuizzThemeController = require("../controllers/quizzTheme.controller");
+  const controller = QuizzThemeController(QuizzThemeService());
+  const authRole = require("../middlewares/authRole");
 
-	router.get("/", controller.getAll);
-	router.post("/", controller.create);
+  router.get("/", controller.getAll);
+  router.post("/", authRole("admin"), controller.create);
 
-	router.get("/:id", controller.getOne);
-	router.put("/:id", controller.replace);
-	router.patch("/:id", controller.update);
-	router.delete("/:id", controller.delete);
+  router.get("/:id", controller.getOne);
+  router.put("/:id", authRole("admin"), controller.replace);
+  router.patch("/:id", authRole("admin"), controller.update);
+  router.delete("/:id", authRole("admin"), controller.delete);
 
-	return router;
+  return router;
 };
