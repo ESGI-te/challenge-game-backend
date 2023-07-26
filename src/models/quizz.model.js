cc759e53ccbd19e3c914f5d8e4665094cf1c5bbf
tlugat <teo.lugat@ausha.co>
2023-07-26 18:13:21 +0200

    ✨ Admin dashboard

--- src/models/quizz.model.js
+++ src/models/quizz.model.js
@@ -5,25 +5,39 @@ const mongoose = require("mongoose");
  */
 
 const QuizzSchema = new mongoose.Schema({
+	difficulty: {
+		type: Number,
+		required: true,
+	},
 	theme: {
 		type: mongoose.Schema.Types.ObjectId,
 		required: true,
 		ref: "QuizzTheme",
 	},
-	name: { type: String, required: true },
+	name: { type: String, required: true, unique: true },
 	slogan: { type: String, required: true },
-	quizz: {
-		type: [
-			{
-				id: { type: Number, required: true },
-				question: { type: String, required: true },
-				propositions: { type: [String], required: true },
-				answer: { type: String, required: true },
-				anecdote: { type: String, required: true },
-			},
-		],
-		required: true,
-	},
+	questions: [
+		{
+			id: { type: Number },
+			question: { type: String, required: true },
+			propositions: { type: [String], required: true },
+			answer: { type: String, required: true },
+		},
+	],
+});
+
+/**
+ * Pre-save middleware to increment the question id
+ */
+
+QuizzSchema.pre("save", function (next) {
+	if (this.isNew) {
+		// Increment the question id for each question in the quizz array
+		this.quizz.forEach((question, index) => {
+			question.id = index + 1;
+		});
+	}
+	next();
 });
 
 /**
