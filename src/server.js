a3998cb72ac18d27e45e4b4548a137e7f5303f96
Chakib <karmimchakib@gmail.com>
2023-07-28 02:58:43 +0200

    allow cors for http requests

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ dayjs().format();
 dotenv.config();
 require("./db")();
 
-app.use(cors());
+app.use(cors({origin : '*' }));
 
 const server = app.listen(3000, () => {
   console.log("App listening on port 3000!");
